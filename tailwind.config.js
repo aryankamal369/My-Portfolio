84ed505ed474c1baf39961cef2f61fec1f496288
Aryan Kamal <113275639+aryankamal369@users.noreply.github.com>
2024-08-25 15:59:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme:{
+        extend:{
+        gridTemplateColumns:{
+            'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        },   
+        fontFamily:{
+           Outfit: ["Outfit", "sans-serif"],
+           Ovo: ["Ovo", "serif"]
+    },
+    animation:{
+        spin_slow: 'spin 6s linear infinite'
+    },
+    colors:{
+        lightHover: '#fcf4ff',
+        darkHover: '#2a004a',
+        darkTheme:'#11001f'
+    }
+    }
+},
+darkMode: 'selector'
+}
